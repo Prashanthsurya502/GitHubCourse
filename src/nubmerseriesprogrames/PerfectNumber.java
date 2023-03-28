a9affbe666fc075402a68bcb2f7d96e34123c658
Prashanthsurya502 <prashanthsurya502@gmail.com>
2023-03-28 08:40:24 +0530

    updated

--- src/nubmerseriesprogrames/PerfectNumber.java
+++ src/nubmerseriesprogrames/PerfectNumber.java
@@ -1,5 +1,28 @@
 package nubmerseriesprogrames;
 
+import java.util.Scanner;
+
 public class PerfectNumber {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value for N= ");
+		int n=s.nextInt();
+		int sum=0;
+		int i=1;
+		while(i<n) {
+			if(n%i==0) {
+				sum=sum+i;
+			}
+			i++;
+		}
+		if (n==sum) {
+			System.out.println(n+" is a perfect number");
+		}
+		else
+		{
+			System.out.println(n+" is not a perfect nubmer");
+		}
+		
+	}
 
 }
