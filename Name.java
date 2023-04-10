1452bdff90f5b1502700cb89bfa7761213c4694a
Prashanthsurya502 <119780773+Prashanthsurya502@users.noreply.github.com>
2023-04-10 07:58:22 +0530

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,10 @@
+public class Name {
+public static void names()
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("please enter your name");
+		String A=s.nextLine();
+		System.out.println(A);
+		
+	}
+}
