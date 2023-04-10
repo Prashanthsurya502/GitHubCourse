a6838daaca66ea6bfd9de708fc6f42b349527027
Prashanthsurya502 <119780773+Prashanthsurya502@users.noreply.github.com>
2023-04-10 08:00:13 +0530

    Create Name.java

---
+++ src/nubmerseriesprogrames/Name.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Name {
+	public static void names()
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("please enter your name");
+		String A=s.nextLine();
+		System.out.println(A);
+		
+	}
+
+}
